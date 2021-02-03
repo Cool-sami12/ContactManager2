008fed70981a3b3da47038686560b91a76b81435
Samuel Aniefiok <coolgen4@outlook.com>
2021-02-03 15:12:48 +0100

    swagger added

---
+++ ContactManager2/Controllers/TokenController.cs
@@ -0,0 +1,17 @@
+﻿using ContactManager2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+     {
+    }
+}
